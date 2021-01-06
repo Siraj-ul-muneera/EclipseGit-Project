9eee42a9dd71905cbeee8c75c80d96ec2ccce113
Siraj-ul-muneera <samreenmuneera@gmail.com>
2021-01-06 21:44:55 +0000

    First Commit in the Eclipse Git Project

---
+++ EclipseGitProject/src/EclipseGitProjectClass.java
@@ -0,0 +1,10 @@
+
+public class EclipseGitProjectClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is an Eclipse Project");
+		// TODO Auto-generated method stub
+
+	}
+
+}
